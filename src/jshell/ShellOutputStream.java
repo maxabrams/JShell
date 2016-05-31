05d5fad4c388a80d3ddb94e5110e256adf3023d4
maxwell <maxwellabrams1@gmail.com>
2016-05-31 09:20:23 -0700

    add comments to ShellOutputStream

--- src/jshell/ShellOutputStream.java
+++ src/jshell/ShellOutputStream.java
@@ -6,16 +6,22 @@ import java.io.OutputStream;
 import javax.swing.JTextArea;
 
 public class ShellOutputStream extends OutputStream{
+	
 	private JTextArea textArea;
 	
+	/**
+	 * Constructor for output stream
+	 * @param text the JTextArea to send the output to
+	 */
 	public ShellOutputStream(JTextArea text){
 		textArea = text;
 	}
 	
 	@Override
 	public void write(int b) throws IOException {
+		//write the output to the text area by appending it to text area
 		textArea.append(String.valueOf((char)b));
+		//move caret to last value for later writing to text area
 		textArea.setCaretPosition(textArea.getDocument().getLength());
 	}
-
 }
