842797d27ee6d57c7fff6343178e73dbd9c60686
maxwell <maxwellabrams1@gmail.com>
2016-05-31 09:09:19 -0700

    comment pwd

--- src/jshell/Pwd.java
+++ src/jshell/Pwd.java
@@ -4,20 +4,17 @@ public class Pwd implements Executable {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "pwd";
 	}
 
 	@Override
 	public String getAbout() {
-		// TODO Auto-generated method stub
 		return getName() + " - Prints current directory";
 	}
 
 	@Override
 	public void execute(String[] currDir, String[] args) {
+		//Format and print current directory reported by the shell
 		System.out.print("\n"+currDir[0]+"/");
-
 	}
-
 }
