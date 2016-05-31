f3705e88f3c868d3b4d957dc20885d4f2fa429c0
maxwell <maxwellabrams1@gmail.com>
2016-05-31 08:42:20 -0700

    add comments to Clear

--- src/jshell/Clear.java
+++ src/jshell/Clear.java
@@ -5,27 +5,28 @@ import javax.swing.JTextArea;
 public class Clear implements Executable {
 
 	private JTextArea outputView;
-	
+
+	/*
+	 * Constructor for Clear
+	 * @param outputView The JTextArea that will be cleared when executed
+	 */
 	public Clear(JTextArea outputView){
 		this.outputView = outputView;
 	}
-	
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "clear";
 	}
 
 	@Override
 	public String getAbout() {
-		// TODO Auto-generated method stub
 		return getName() + " - method to clear the shell";
 	}
 
 	@Override
 	public void execute(String[] currDir, String[] args) {
+		//Clear output when executed by setting text to empty
 		outputView.setText("");
-
 	}
-
 }
