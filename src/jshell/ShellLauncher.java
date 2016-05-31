0ef920040a603c31d344f1caab66f9d2075bddf4
maxwell <maxwellabrams1@gmail.com>
2016-05-31 09:18:39 -0700

    add comments to shell launcher

--- src/jshell/ShellLauncher.java
+++ src/jshell/ShellLauncher.java
@@ -2,8 +2,9 @@ package jshell;
 
 public class ShellLauncher {
 
+	//Main running of the shell
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Shell shell1 = new Shell();
+		@SuppressWarnings("unused")//since we are manipulating GUI when it shows up
+		Shell shell1 = new Shell();//create new shell and run it
 	}
 }
