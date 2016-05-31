2c7e81973b0528c9db0d8778b8a767588dd2b405
maxwell <maxwellabrams1@gmail.com>
2016-05-31 08:56:47 -0700

    add comments to file system

--- src/jshell/FileSystem.java
+++ src/jshell/FileSystem.java
@@ -7,41 +7,66 @@ import java.io.PrintStream;
 public class FileSystem {
 	private String rootPath;
 	private String root;
-	//private String currDir;
 
+	/**
+	 * Constructor for FileSystem
+	 * @param rootFolder the name of the root (home directory) to use
+	 * @param stream the output to print information to
+	 */
 	public FileSystem(String rootFolder, PrintStream stream){
 		root = rootFolder;
 		System.setOut(stream);
+		//If the root is missing or has never been created, make a new root directory
 		createRootIfMissing();
-		//currDir = root;
 	}
 
+	/**
+	 * Returns the root directory name
+	 * @return the root directory name
+	 */
 	public String getRoot(){
 		return root;
 	}
 
+	/**
+	 * Method to get the path of the root folder on the real file system
+	 * @return the whole "real" path up to root 
+	 */
 	public String getRootPath(){
 		return rootPath;
 	}
+
+	/**
+	 * Method to create root file if non-existent
+	 */
 	private void createRootIfMissing(){
+		//For this project, we will always place the "root" folder on the desktop
 		rootPath = System.getProperty("user.home") + File.separator + "Desktop" + File.separator;
 		File rootFolder = new File(rootPath + root);
-		if(!rootFolder.exists()){
+		if(!rootFolder.isDirectory()){
 			//No root folder, will need to create it
 			rootFolder.mkdir();
 		}
-
 	}
 
+	/**
+	 * Method to create a file on the file system
+	 * @param currDir user current directory
+	 * @param path location of where to place the new file if possible
+	 */
 	public void createFile(String currDir, String path){
 		if(path.startsWith(root)||path.split(File.separator).length==1){
+			//Must be inside of the root directory or inside of current directory
 			File newFile;
 			if(!path.startsWith(root)){
+				//create file in current directory
 				newFile = new File(rootPath + currDir + File.separator + path);	
 			}else{
+				//create a file under root (user used full path)
 				newFile = new File(rootPath + path);
 			}
 			try {
+				//create the file
 				newFile.createNewFile();
 			} catch (IOException e) {
 				System.out.print("\nError: Cannot create file: " + path);
@@ -50,25 +75,42 @@ public class FileSystem {
 			System.out.print("\nError: Cannot create file outside of root path");
 		}
 	}
-	
+
+	/**
+	 * Method to check if a file exists in the file system
+	 * @param currDir user current directory 
+	 * @param path location of where to check for the file
+	 * @return boolean for if the file exists (true) or not (false)
+	 */
 	public boolean doesFileExist(String currDir, String path){
-				File newFile;
-				if(!path.startsWith(root)){
-					newFile = new File(rootPath + currDir + File.separator + path);	
-				}else{
-					newFile = new File(rootPath + path);
-				}
-				
-			return newFile.isFile();
-		
+		File newFile;
+		if(!path.startsWith(root)){
+			//must be directory in current directory
+			newFile = new File(rootPath + currDir + File.separator + path);	
+		}else{
+			//full path given, check full path
+			newFile = new File(rootPath + path);
+		}
+
+		//Check if path gives a file
+		return newFile.isFile();	
 	}
-	
+
+	/**
+	 * Method to check get a file if it exists in the file system
+	 * @param currDir user current directory 
+	 * @param path location of where to check for the file
+	 * @return the file if it exists in the file system. If not, a null file is returned
+	 */
 	public File getFile(String currDir, String path){
 		if(doesFileExist(currDir, path)){
+			//File must exist
 			File newFile;
-			if(!path.startsWith(root)){		
+			if(!path.startsWith(root)){	
+				//must be directory in current directory
 				newFile = new File(rootPath + currDir + File.separator + path);	
 			}else{
+				//full path given, use full path
 				newFile = new File(rootPath + path);
 			}
 			return newFile;
@@ -76,74 +118,39 @@ public class FileSystem {
 		System.out.print("\nError: Cannot get file: "+ path);
 		return null;
 	}
-	
 
+
+	/**
+	 * Create a folder given a path if it is within root structure
+	 * @param currDir user current directory 
+	 * @param path location of where to create the folder
+	 */
 	public void createFolder(String currDir, String path){
 		if(path.startsWith(root)||path.split(File.separator).length==1){
 			//full path specified or only a name given
 			File newFile;
 			boolean didCreate = false;
 			if(!path.startsWith(root)){
+				//must want to create in current directory
 				newFile = new File(rootPath + currDir + File.separator + path);	
 			}else{
+				//full path given, use full path to create
 				newFile = new File(rootPath + path);
 			}
 			try {
+				//create path
 				didCreate = newFile.mkdir();
 			} catch (SecurityException e) {
 				System.out.print("\nError: Cannot create dirs: " + path);
 			}
-			
+
 			if(didCreate == false){
+				//folders in path are missing, need to create those first
 				System.out.print("\nError: Cannot create directory since missing directory in path: " + path);
 			}
 		}else{
+			//Was outside of root path
 			System.out.print("\nError: Cannot create folder outside of root path");
 		}
 	}
-
-	/*
-	public void changeDir(String path){
-		System.out.print(currDir);
-		if (path.equals("..")){
-			//need to go up a directory
-			if(!currDir.equals(root+File.separator)){
-				//cannot go past root
-				String[] parsePath = currDir.split(File.separator);
-				//Loop and re-create path
-				currDir = "";
-				for(int i = 0; i < parsePath.length-1;i++){
-					if(i==parsePath.length-1){
-						currDir = currDir + parsePath[i];
-					}else{
-						currDir = currDir + parsePath[i] + File.separator;
-					}
-				}
-				//
-			}
-			return;
-		}
-
-		if(path.startsWith(root)||path.split(File.separator).length==1){
-			//full path specified or only a name given
-			File newFile;
-			if(!path.startsWith(root)){
-				//Must be a folder inside curr dir
-				newFile = new File(rootPath + currDir + File.separator + path);	
-			}else{
-				//change to full path
-				newFile = new File(rootPath + path);
-			}
-			boolean canFind = newFile.exists();
-			if(canFind){
-				currDir = currDir + File.separator + path;
-			}
-			else{
-				System.out.print("\nError: Cannot cannot find dir: " + path);
-			}
-		}else{
-			System.out.print("\nError: Cannot move to folder outside of root path");
-		}
-		System.out.print(currDir);
-	}*/
 }
