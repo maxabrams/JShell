befa8e922d3ca55d69576a055aade51f5251adff
maxwell <maxwellabrams1@gmail.com>
2016-05-31 09:03:10 -0700

    add comments to ls
    fix ls to also look at whole path given

--- src/jshell/Ls.java
+++ src/jshell/Ls.java
@@ -5,9 +5,14 @@ import java.io.File;
 public class Ls implements Executable {
 	FileSystem sys;
 
+	/**
+	 * Constructore for LS
+	 * @param system file system to report on
+	 */
 	public Ls(FileSystem system){
 		sys = system;
 	}
+
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -30,10 +35,21 @@ public class Ls implements Executable {
 				System.out.print("\n"+listOfFiles[i].getName());
 			}
 		}else{
-			File newFile = new File(sys.getRootPath() + sys.getRoot() + File.separator + args[0]);
-			if(!newFile.exists()){
+			//list files in specified path of directories
+			File newFile;
+			if(!args[0].startsWith(sys.getRoot())){
+				//must want to create in current directory
+				newFile = new File(sys.getRootPath() + sys.getRoot() + File.separator + args[0]);
+			}else{
+				//full path given, use full path to create
+				newFile = new File(sys.getRootPath() + args[0]);
+			}
+
+			if(!newFile.isDirectory()){
+				//Not a valid directory
 				System.out.println("\nError: No directory found: " + args[0]);
 			}else{
+				//Valid directory, print all files in directory
 				File[] listOfFiles = newFile.listFiles();
 				for(int i = 0; i < listOfFiles.length;i++){
 					System.out.print("\n"+listOfFiles[i].getName());
@@ -41,7 +57,6 @@ public class Ls implements Executable {
 			}
 		}
 	}
-
 }
 
 
