6080cd56978f379e1abee31958c596a7003be4af
maxwell <maxwellabrams1@gmail.com>
2016-05-31 08:40:11 -0700

    comment CD

--- src/jshell/Cd.java
+++ src/jshell/Cd.java
@@ -5,46 +5,51 @@ import java.io.File;
 public class Cd implements Executable {
 
 	FileSystem sys;
+	/*
+	 * Constructor for Cd
+	 * @param sys FileSystem used for directory manipulation
+	 */
 	public Cd(FileSystem sys){
 		this.sys = sys;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "cd";
 	}
 
 	@Override
 	public String getAbout() {
-		// TODO Auto-generated method stub
 		return getName() + " - change to given directory";
 	}
 
 	@Override
 	public void execute(String[] currDir, String[] args) {
+		//Make sure a directory to change into is given
 		if(args.length<1){
 			System.out.print("\nError: Please supply a directory name");
 		}else{
-			//public void changeDir(String path){
+			//Path is given, parse the path for manipulation
 			String path = args[0];
 			String root = sys.getRoot();
 			String rootPath = sys.getRootPath();
 			if(path.startsWith(root)&&!path.startsWith(root+File.separator)){
+				//Found that user wants to change to root, but does not have file separator indicating want to be "inside" of folder
 				System.out.print("\nError: please add file serpator to root folder");
 				return;
 			}
-			//TESTING before and after System.out.print("prev"+currDir[0]);
+
 			if (path.equals("..")){
-				//need to go up a directory
+				//Need to go up a directory
 				if(!currDir[0].equals(root+File.separator)){
-					//cannot go past root
+					//Cannot go past root
 					String[] parsePath = currDir[0].split(File.separator);
 					//Loop and re-create path
-					//currDir[0] = sys.getRoot() + File.separator;
 					if(parsePath.length<=2){
-						currDir[0] = sys.getRoot(); //+ File.separator;// currDir[0] + parsePath[0];
+						//If parsed path has a length less than or equal to two, user must want to be in highest directory
+						currDir[0] = sys.getRoot(); 
 					}else{
+						//Parse whole given path
 						currDir[0] = sys.getRoot() + File.separator;
 						for(int i = 1; i < parsePath.length-1;i++){
 							if(i==parsePath.length-2){
@@ -54,32 +59,35 @@ public class Cd implements Executable {
 							}
 						}
 					}
-					//
 				}
-				//TESTING System.out.print("fin"+currDir[0]);
 				return;
-
 			}
 
 			if(path.startsWith(root)||path.split(File.separator).length==1){
-
-				//full path specified or only a name given
+				//Full path specified or only a name given
 				File newFile;
 				if(!path.startsWith(root)){
-					//Must be a folder inside curr dir
+					//Must be a folder inside curr directory
 					newFile = new File(rootPath + currDir[0] + File.separator + path);	
 				}else{
-					//change to full path
+					//Change to full path
 					newFile = new File(rootPath + path);
 				}
+
+				//Check if specified directory exists to change into
 				boolean canFind = newFile.isDirectory();
+
 				if(canFind){
+					//Exists
 					if(path.startsWith(root+File.separator)){
+						//Must be higher directory
 						if(path.endsWith("/")){
+							//format user input to exclude "/"
 							path = path.substring(0, path.length()-1);
 						}
 						currDir[0] = path;
 					}else{
+						//Must be lower directory
 						currDir[0] = currDir[0] + File.separator + path;
 					}
 				}
@@ -89,9 +97,7 @@ public class Cd implements Executable {
 			}else{
 				System.out.print("\nError: Cannot move to folder outside of root path");
 			}
-			//TESTING before and after System.out.print("fin"+currDir[0]);
 		}
 	}
-
 }
 
